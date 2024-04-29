fd58af259739e98e7028b45942135d60801d4121
Sunny Tsai <48792220+sunnytsai3346@users.noreply.github.com>
2024-04-29 14:53:24 -0400

    add color input

--- angular-component-viewchild-demo/src/app/app.component.ts
+++ angular-component-viewchild-demo/src/app/app.component.ts
@@ -1,26 +1,39 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, ViewChild, ViewChildren, viewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChild, ViewChildren, viewChild } from '@angular/core';
 import { Todo } from './model/Todo';
 import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: [
+    `.completed {
+      text-decoration: line-through;
+    }`
+  ],
 })
 export class AppComponent implements AfterViewInit {
   title = 'ViewChild Demo';  
-  @ViewChild(TodoComponent) child?:TodoComponent = new TodoComponent;
+  @ViewChild(TodoComponent) child?:TodoComponent = new TodoComponent(this.renderer);
+  //View Children
+  @ViewChildren("childheading") childheading!: QueryList<any>;
   
   parentMessage: string ="Let's add new item ..."; 
+  color:string="red";
   todos : Array<Todo> = [];
   model!:Todo ;
 
-  constructor(){
+  constructor(private renderer: Renderer2){
     this.todos=[];
   }
   
   ngAfterViewInit(): void {
     this.model = this.child!.model;
+    console.log(this.childheading)
+    //ViewChildren
+    this.childheading.forEach(item => {
+      this.renderer.setStyle(item.nativeElement, 'color', 'blue')
+    })
+    
   }
   
   addTodo(res : Todo)
--- angular-component-viewchild-demo/src/app/todo/todo.component.ts
+++ angular-component-viewchild-demo/src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { Todo } from '../model/Todo';
 
 @Component({
@@ -9,16 +9,29 @@ import { Todo } from '../model/Todo';
 export class TodoComponent {
    
   @Input() parentMessageData: string ="";
+  @Input() color: string ="";
   @Output() messageEvent:EventEmitter<Todo> = new EventEmitter<Todo>();
   model:Todo;  
+  //View Children
+  @ViewChildren("childheading") childheading!: QueryList<any>;
   
-  constructor(){
+  constructor(private renderer:Renderer2){
     this.model= {      
       task:"",
       complete:false,
     }
   }
 
+  ngAfterViewInit(): void {
+    
+    console.log(this.childheading)
+    //ViewChildren
+    this.childheading.forEach(item => {
+      this.renderer.setStyle(item.nativeElement, 'color', this.color)
+    })
+    //this.renderer.setStyle(this.childheading.last.nativeElement, 'color', 'blue');
+
+  }
   taskChange(event:KeyboardEvent){
     this.model.task = (event.target as HTMLInputElement).value;
     
